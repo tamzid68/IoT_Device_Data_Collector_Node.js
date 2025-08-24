54e817606e44ea9d026c88c778197c684301ae7b
ASM Tamzid <atkthegreat9999@gmail.com>
2025-08-25 00:55:10 +0600

    fix query problem

--- src/controllers/reading.controller.ts
+++ src/controllers/reading.controller.ts
@@ -1,25 +1,28 @@
 import { Request, Response } from 'express';
 import logger from '../utils/logger.utils';
 import { findReadings } from '../services/reading.service';
-import { ReadingParams } from '../models/reading.model';
 
 
 export const getReadings = async (req: Request, res: Response) => {
     try {
-        const { device_id} = req.query;
+         const queryParams = {
+        device_id: req.query.device_id as string,
+        start_time: req.query.start_time as Date | undefined,
+        end_time: req.query.end_time as Date | undefined,
+        limit: req.query.limit as number | undefined,
+        sort: req.query.sort as 'asc' | 'desc' | undefined,
+    };
+        //const { device_id} = req.query;
 
         // if (!device_id || typeof device_id !== 'string') {
         //     return res.status(400).json({ error: 'device_id query parameter is required.' });
         // }
 
-        const params: ReadingParams = { device_id: device_id as string };
-
-    
-
-        const readings = await findReadings(params);
+        //const params: ReadingParams = { device_id: device_id as string };
+        const readings = await findReadings(queryParams);
 
         res.status(200).json({
-            device_id: device_id,
+            device_id: queryParams.device_id,
             count: readings.length,
             readings: readings
         });
