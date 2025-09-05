1b6d3123f9894e5fb3d911ef0a545863a78568c0
ASM Tamzid <atkthegreat9999@gmail.com>
2025-09-06 01:46:44 +0600

    improvement in validation rules a bit stricter and more explicit

--- src/validators/data.validator.ts
+++ src/validators/data.validator.ts
@@ -1,27 +1,33 @@
-import {body} from 'express-validator';
-import {handleValidationErrors} from '../middleware/validation.middleware';
+import { body } from 'express-validator';
+import { handleValidationErrors } from '../middleware/validation.middleware';
 
 /**
  * An array of validation middleware for the data ingestion endpoint.
  */
-export const ingestDataValidator =[
+export const ingestDataValidator = [
     //Custom validator to ensure at least one sensor reading is present
-    body().custom((value, {req})=>{
-        const {temperature, humidity} = req.body;
-        if(temperature === undefined && humidity === undefined){
+    body().custom((value, { req }) => {
+        const { temperature, humidity } = req.body;
+        if (temperature === undefined && humidity === undefined) {
             throw new Error('Payload must contain at least one sensor reading (e.g., temperature, humidity).');
         }
         return true;
     }),
 
-     // Validate 'temperature' if it exists. `checkFalsy: true` allows 0 as a valid value.
-    body('temperature').optional({ checkFalsy: true }).isNumeric().withMessage('Temperature must be a number.'),
+    // Validate 'temperature' if it exists. `checkFalsy: true` allows 0 as a valid value.
+    body('temperature')
+        .optional({ checkFalsy: true })
+        .isFloat({ min: -10.0, max: 60.0 }).withMessage('Temperature must be a number.'),
 
     // Validate 'humidity' if it exists.
-    body('humidity').optional({ checkFalsy: true }).isNumeric().withMessage('Humidity must be a number.'),
+    body('humidity')
+        .optional({ checkFalsy: true })
+        .isFloat({ min: 0.0, max: 100.0 }).withMessage('Humidity must be a number.'),
 
     // Validate 'timestamp' if it exists and convert it to a Date object.
-    body('timestamp').optional().isISO8601().withMessage('If provided, timestamp must be a valid ISO 8601 date string.').toDate(),
+    body('timestamp')
+        .optional().isISO8601()
+        .withMessage('If provided, timestamp must be a valid ISO 8601 date string.').toDate(),
 
     // This must be the last item in the array to process the validation results.
     handleValidationErrors,
